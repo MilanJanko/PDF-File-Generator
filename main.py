97cc607a09de4f04d338f4bcbf18ffc31c0f340f
MilanJanko <miljankovic91@gmail.com>
2023-08-13 13:25:03 +0200

    Added footer - Page number

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('topics.csv', sep=',')
 
 pdf = FPDF(orientation='portrait', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -14,7 +15,22 @@ for index, row in df.iterrows():
              ln=1)
     pdf.line(10, 21, 200, 21)
 
+    #Set footer
+    pdf.ln(265)
+    pdf.set_font(family='Times', style='I', size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=str(1), align='R')
+
+    page_num = 2
+
     for i in range(row['Pages']-1):
         pdf.add_page()
+
+        # Set footer
+        pdf.ln(277)
+        pdf.set_font(family='Times', style='I', size=10)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=str(page_num), align='R')
+        page_num += 1
 
 pdf.output('output_file.pdf')
